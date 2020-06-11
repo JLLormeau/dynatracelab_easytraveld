548c6b4ca470dac7e129802b775cfed85a0a8be3
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2020-06-11 10:36:06 +0200

    Update start-stop-easytavel.sh

--- start-stop-easytavel.sh
+++ start-stop-easytavel.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
+### BEGIN INIT INFO
+# Provides: skeleton
+# Required-Start: $remote_fs $syslog
+# Required-Stop: $remote_fs $syslog
+# Should-Start: $portmap
+# Should-Stop: $portmap
+# X-Start-Before: nis
+# X-Stop-After: nis
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# X-Interactive: true
+# Short-Description: Example initscript
+# Description: This file should be used to construct scripts to be placed in /etc/init.d.
+#
+### END INIT INFO
 
 case "$1" in
 	start)
 		echo "Starting EasyTravel..."
-    cd dynatracelab_easytraveld
-    sudo docker-compose up -d
+    		cd /home/dynatracelab_easytraveld
+   		docker-compose down
+		docker-compose up -d
 		;;
 	stop)
 		echo "Stopping EasyTravel..."
 		docker-compose down
 		;;
-	restart)
-		$0 stop
-		sleep 1
-		$0 start
-		;;
 	*)
-		echo "Usage: $0 {start|stop|restart}"
+		echo "Usage: $0 {start|stop}"
 		exit 1
 		;;
 esac
